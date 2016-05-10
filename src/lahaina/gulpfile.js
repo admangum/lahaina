ba2809838f07a8fc602642444b97c0d94681e21a
admangum <adam.mangum@autodesk.com>
2016-05-10 13:48:03 -0700

    Adds autoprefixer to gulp sass task

--- src/lahaina/gulpfile.js
+++ src/lahaina/gulpfile.js
@@ -9,6 +9,8 @@ var gulp = require('gulp'),
 	source = require('vinyl-source-stream'),
 	buffer = require('vinyl-buffer'),
 	sass = require('gulp-sass'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
 	rename = require('gulp-rename'),
 	babel = require('gulp-babel'),
 	webpack = require('webpack-stream'),
@@ -26,36 +28,6 @@ paths = {
 	}
 };
 
-// opts = assign({}, watchify.args, {
-// 	entries: ['./src/main.js'],
-// 	debug: true
-// });
-
-// bundler = watchify(browserify(opts));
-// bundler = browserify({
-// 	entries: ['./src/main.js'],
-// 	debug: true
-// });
-// bundler
-// 	.on('update', bundle)
-// 	.on('log', gutil.log);
-
-
-// function bundle(){
-// 	return bundler
-// 		// .transform('babelify', {presets: ['es2015']})
-// 		.transform('reactify', {es6: true})
-// 		.bundle()
-// 		// .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-// 		.pipe(source('bundle.js'))
-// 		// .pipe(buffer())
-// 		// .pipe(maps.init())
-// 		// .pipe(maps.write('./'))
-// 		.pipe(gulp.dest('./dist/'));
-// }
-
-// gulp.task('browserify', bundle);
-
 gulp.task('build', function(){
 	return gulp.src('./src/main.js')
 		.pipe(webpack(require('./webpack.config.js')).on('error', function(err){ console.log(err)}))
@@ -65,11 +37,12 @@ gulp.task('build', function(){
 gulp.task('sass', function(){
 	gulp.src('./src/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
 		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('deploy', function(){
-	return gulp.src('./dist/**/*')
+	return gulp.src(['./dist/**/*.js', './dist/style.css'])
 		.pipe(gulp.dest('../../www/wp-content/themes/lahaina/'));
 });
 

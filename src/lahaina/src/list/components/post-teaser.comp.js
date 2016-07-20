ffb1a4c460293456308d54bd5fc952026f3d1c6f
admangum <adam.mangum@autodesk.com>
2016-07-19 22:44:56 -0700

    Adds summary field to post teasers

--- src/lahaina/src/list/components/post-teaser.comp.js
+++ src/lahaina/src/list/components/post-teaser.comp.js
@@ -30,6 +30,12 @@ module.exports = React.createClass({
 				style.transform = 'translate(' + layout.x + 'px,' + layout.y + 'px)';
 			};
 
+		function getExcerpt(){
+			if(data.custom_fields.summary && data.custom_fields.summary.length){
+				return data.custom_fields.summary[0];
+			}
+		}
+
 		return (<li ref={data.id} className={className} style={style} onClick={this.onClick}>
 					{imgUtils.getTeaserImage(data, {width: config.layout.minColWidth})}
 					<PostTags data={data.categories} type="category" />
@@ -37,7 +43,7 @@ module.exports = React.createClass({
 						<span dangerouslySetInnerHTML={{__html: data.title}}></span>
 					</h2>
 					<PostTags data={data.tags} type="tag" />
-					<div dangerouslySetInnerHTML={{__html: data.excerpt}}/>
+					<p dangerouslySetInnerHTML={{__html: getExcerpt()}}/>
 				</li>);
 	}
 });

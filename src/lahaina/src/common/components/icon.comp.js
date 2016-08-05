d0a8490f311165ba574b2c6ccedb16bd6d4221a8
admangum <adam.mangum@autodesk.com>
2016-08-05 00:31:18 -0700

    Clean up

--- src/lahaina/src/common/components/icon.comp.js
+++ src/lahaina/src/common/components/icon.comp.js
@@ -1,14 +1,5 @@
 var React = require('react');
 
-// class Icon extends React.Component {
-// 	render(){
-// 		var glyph = this.props.glyph;
-// 		return (
-// 			<svg className="icon" dangerouslySetInnerHTML={{__html: '<use xlink:href="'+glyph+'"></use>'}} />
-// 		);
-// 	}
-// }
-
 module.exports = ({glyph}) => (
 		<svg className="icon" dangerouslySetInnerHTML={{__html: '<use xlink:href="'+glyph+'"></use>'}} />
 	);

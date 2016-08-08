b5e4ff8b48b171d7a2b8706614b48f5b7a5f92c6
admangum <adam.mangum@autodesk.com>
2016-08-07 21:32:20 -0700

    Styles, layout

--- src/lahaina/src/app.comp.js
+++ src/lahaina/src/app.comp.js
@@ -1,14 +1,42 @@
 var React = require('react');
+var Reflux = require('reflux');
 var IndexLink = require('react-router').IndexLink;
+var Footer = require('./footer/components/footer.comp');
+var PostStore = require('./core/stores/post.store');
 
 module.exports = React.createClass({
+	mixins: [Reflux.ListenerMixin],
+	componentWillMount: function(){
+		this.listenTo(PostStore, this.onPostsChange);
+	},
+	onPostsChange: function(data){
+		if(data.postData){
+			this.setState({
+				posts: data.postData.posts,
+				selectedPost: data.selectedPost
+			});
+		}
+	},
+	getInitialState: function(){
+		return {
+			posts: [],
+			selectedPost: null
+		};
+	},
+	getFooter: function(posts){
+		if(posts.length){
+			return (<Footer list={posts} />);
+		}
+	},
 	render: function(){
+		var posts = this.state.posts || [];
 		return (
 			<div>
 				<header>
 					<h1><IndexLink to="1">*</IndexLink></h1>
 				</header>
 				{this.props.children}
+
 			</div>
 		);
 	}
--- src/lahaina/src/footer/components/footer.comp.js
+++ src/lahaina/src/footer/components/footer.comp.js
@@ -44,45 +44,53 @@ module.exports = React.createClass({
 		return (content && <footer id="colophon" role="contentinfo">
 	  <div className="inner">
 	  	<div className="featured section">
-			<h3>Featured</h3>
-			<div className="post-teaser-list">
-				<ul>
-					{this.getTeasers(content.posts[0])}
-				</ul>
-			</div>
-			<div className="post-teaser-list">
-				<ul>
-					{this.getTeasers(content.posts[1])}
-				</ul>
+	  		<div className="inner">
+				<h3>Featured</h3>
+				<div className="post-teaser-list">
+					<ul>
+						{this.getTeasers(content.posts[0])}
+					</ul>
+				</div>
+				<div className="post-teaser-list">
+					<ul>
+						{this.getTeasers(content.posts[1])}
+					</ul>
+				</div>
 			</div>
 		 </div>
 		 <div className="taxonomy section">
-		 	<div className="categories">
-			 	<h3>Categories</h3>
-			 	<ul className="category-list post-taxonomy-list">
-			 		{this.getTaxonomyItems(content.categories, 'category')}
-			 	</ul>
-		 	</div>
-		 	<div className="tags">
-			 	<h3>Tags</h3>
-			 	<ul className="tag-list post-taxonomy-list">
-			 		{this.getTaxonomyItems(content.tags, 'tag')}
-			 	</ul>
+		 	<div className="inner">
+			 	<div className="categories">
+				 	<h3>Categories</h3>
+				 	<ul className="category-list post-taxonomy-list">
+				 		{this.getTaxonomyItems(content.categories, 'category')}
+				 	</ul>
+			 	</div>
+			 	<div className="tags">
+				 	<h3>Tags</h3>
+				 	<ul className="tag-list post-taxonomy-list">
+				 		{this.getTaxonomyItems(content.tags, 'tag')}
+				 	</ul>
+			 	</div>
 		 	</div>
 		 </div>
 		 <div className="about section">
-		 	<h3>What is this?</h3>
-		 	<p>I'm Adam, this is my place to post things I'm working on, interested in, or whatever!</p>
+		 	<div className="inner">
+			 	<h3>What is this?</h3>
+			 	<p>I'm Adam, this is my place to post things I'm working on, interested in, or whatever!</p>
+		 	</div>
 		 </div>
 		 <div className="elsewhere section">
-		 	<h3>Elsewhere</h3>
-		 	<ul>
-		 		<li>
-		 			<a href="https://github.com/admangum">
-		 				<Icon glyph={require('../../common/img/github-mark.svg')}/>
-		 			</a>
-		 		</li>
-		 	</ul>
+		 	<div className="inner">
+			 	<h3>Elsewhere</h3>
+			 	<ul>
+			 		<li>
+			 			<a href="https://github.com/admangum">
+			 				<Icon glyph={require('../../common/img/github-mark.svg')}/>
+			 			</a>
+			 		</li>
+			 	</ul>
+		 	</div>
 		 </div>
 		<div className="site-info">
 			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform">Proudly powered by WordPress</a>

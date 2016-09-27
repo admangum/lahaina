cf819ac811acbf031aa24ff49943d5d7fa986a66
admangum <adam.mangum@autodesk.com>
2016-09-27 12:13:12 -0700

    Makes embedded content styleable

--- src/lahaina/src/post/components/embedded-content.comp.js
+++ src/lahaina/src/post/components/embedded-content.comp.js
@@ -1,10 +1,40 @@
 var React = require('react');
 
 module.exports = React.createClass({
+    onLoad: function(){
+        this.updateEmbeddedContentBodyStyles();
+    },
+    updateEmbeddedContentBodyStyles: function(){
+        var embeddedContentBody = this.getEmbeddedContentBody(),
+            embeddedContentBodyStyles = this.getEmbeddedContentBodyStyles();
+        if(embeddedContentBody && embeddedContentBodyStyles){
+            _.each(embeddedContentBodyStyles, function(val, key){
+                embeddedContentBody.style[key] = val;
+            });
+        }
+	},
+	getEmbeddedContentBody: function(){
+		var embeddedContent = document.getElementById('embedded-content'),
+			embeddedDocument = embeddedContent && (embeddedContent.contentDocument || embeddedContent.contentWindow.document);
+		return embeddedDocument && embeddedDocument.getElementsByTagName('body')[0];
+	},
+	getEmbeddedContentBodyStyles: function(){
+		var post = this.props.post,
+            styleDef = post.custom_fields.embedded_content_body_styles;
+			styleArr = styleDef && styleDef[0] && styleDef[0].split(';');
+		return _.reduce(styleArr, function(memo, style){
+			style = (style.replace(/\s/g, '') || '').split(':');
+			if(style.length === 2){
+				// style[0] == style name, style[1] == style value
+				memo[style[0]] = style[1];
+			}
+			return memo;
+		}, {});
+	},
     render: function(){
         return (
-            <div className="embedded-content">
-                <iframe src="http://www.nytimes.com" allowfullscreen="0" frameborder="0" />
+            <div className="embedded-content-wrapper">
+                <iframe id="embedded-content" src="/projects/test" allowFullScreen="0" frameBorder="0" onLoad={this.onLoad}/>
             </div>
         );
     }
